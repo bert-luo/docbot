797d1f6cd46703c1fe59f91e173d306132e6b863
Albert <luoa@mit.edu>
2023-11-15 12:01:14 -0800

    server skeleton

---
+++ server/api.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# run using 
+# $ uvicorn api:app --reload
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/chat/")
+def chat(): 
+
+    return {"message": "Hello World"}
