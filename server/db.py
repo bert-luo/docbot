d587e02f98c15439bf09bc897c2f9d49e9ca4ae7
Albert <luoa@mit.edu>
2023-11-16 17:19:57 -0800

    chat endpoint progress again

--- server/db.py
+++ server/db.py
@@ -1,92 +1,125 @@
 import weaviate
 from dotenv import load_dotenv
 import os
+import json
 
 from llm import embed_texts
 
 load_dotenv()
 WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
-print(WEAVIATE_API_KEY)
-
+COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 auth_config = weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY)
 
 client = weaviate.Client(
   url="https://cohere-hack-imnt0qiv.weaviate.network",
-  auth_client_secret=auth_config
-)
-print(client.is_ready())
+  auth_client_secret=auth_config, 
+  additional_headers={
+        "X-Cohere-Api-Key": COHERE_API_KEY,
+    }
 
-document_schema = {
-    "class": "Document",
-    "description": "page or chunk of software documentation",
-    "vectorizer": "text2vec-cohere",
-    "moduleConfig": {
-        "text2vec-cohere": {
-            "model": "embed-english-v3.0", #TODO: use embed-english-light-v3.0 if too slow
-            "truncate": "RIGHT"
-        }, 
-        "generative-cohere": {
-          "model": "command-xlarge-nightly",  # Optional - Defaults to `command-xlarge-nightly`. Can also use`command-xlarge-beta` and `command-xlarge`
-          "temperatureProperty": 0.5,  # Optional
-          #"maxTokensProperty": <maxTokens>,  # Optional
-          #"kProperty": <k>, # Optional
-          #"stopSequencesProperty": <stopSequences>, # Optional
-          #"returnLikelihoodsProperty": <returnLikelihoods>, # Optional
-        },
-    },
-    "vectorIndexConfig": {
-        "distance": "dot"
-    },
-    "properties": [
-    {
-        "name": "text",
-        "dataType": [ "text" ],
-        "description": "Doc body",
+)
+print("client ready: ", client.is_ready())
+
+if False:
+    document_schema = {
+        "class": "Document",
+        "description": "page or chunk of software documentation",
+        "vectorizer": "text2vec-cohere",
         "moduleConfig": {
             "text2vec-cohere": {
-                "skip": False,
-                "vectorizePropertyName": False
+                "model": "embed-english-v3.0", #TODO: use embed-english-light-v3.0 if too slow
+                "truncate": "RIGHT"
+            }, 
+            "generative-cohere": {
+            "model": "command-xlarge-nightly",  # Optional - Defaults to `command-xlarge-nightly`. Can also use`command-xlarge-beta` and `command-xlarge`
+            "temperatureProperty": 0.5,  # Optional
+            #"maxTokensProperty": <maxTokens>,  # Optional
+            #"kProperty": <k>, # Optional
+            #"stopSequencesProperty": <stopSequences>, # Optional
+            #"returnLikelihoodsProperty": <returnLikelihoods>, # Optional
+            },
+        },
+        "vectorIndexConfig": {
+            "distance": "dot"
+        },
+        "properties": [
+        {
+            "name": "text",
+            "dataType": [ "text" ],
+            "description": "Doc body",
+            "moduleConfig": {
+                "text2vec-cohere": {
+                    "skip": False,
+                    "vectorizePropertyName": False
+                }
             }
-        }
-    },
-    {
-        "name": "url",
-        "dataType": [ "text" ],
-        "moduleConfig": { "text2vec-cohere": { "skip": True } }
-    },
+        },
+        {
+            "name": "url",
+            "dataType": [ "text" ],
+            "moduleConfig": { "text2vec-cohere": { "skip": True } }
+        },
+        ]
+    }
+
+    client.schema.delete_all()
+    client.schema.create_class(document_schema)
+
+    client.batch.configure(
+        batch_size=200,
+        dynamic=True,
+        timeout_retries=3,
+    )
+
+    test_documents = [
+        "Rust is the most loved programming language",
+        "Python is an interpreted programming language", 
+        "Pythons are reptiles", 
+        "programming is a lucrative career in the 21st century"
     ]
-}
+    embeddings = embed_texts(test_documents)
 
-client.schema.delete_all()
-client.schema.create_class(document_schema)
+    with client.batch as batch:
+        for doc, embedding in zip(test_documents, embeddings):
+            properties = {
+            "text": doc,
+            "url": str(len(doc)),
+            }
+            vector = embedding
 
-client.batch.configure(
-    batch_size=200,
-    dynamic=True,
-    timeout_retries=3,
-)
+            batch.add_data_object(properties, "Document", None, vector)
 
-test_documents = [
-    "Rust is the most loved programming language",
-    "Python is an interpreted programming language", 
-    "Pythons are reptiles", 
-    "programming is a lucrative career in the 21st century"
-]
-embeddings = embed_texts(test_documents)
+    result = (
+        client.query.aggregate("Document")
+        .with_fields("meta { count }")
+        .do()
+    )
+    print("Object count: ", result["data"]["Aggregate"]["Document"])
 
-with client.batch as batch:
-    for doc, embedding in zip(test_documents, embeddings):
-        properties = {
-        "text": doc,
-        "url": str(len(doc)),
-        }
-        vector = embedding
+if False: # hybrid search
+    response = (
+        client.query
+        .get("Document", ["text", "url"])
+        .with_hybrid(
+            query="coding",
+            alpha=0
+        )
+        #.with_additional(["score", "explainScore"])
+        .with_limit(3)
+        .do()
+    )
 
-        batch.add_data_object(properties, "Document", None, vector)
+    print(json.dumps(response, indent=2))
 
-result = (
-    client.query.aggregate("Document")
-    .with_fields("meta { count }")
-    .do()
-)
-print("Object count: ", result["data"]["Aggregate"]["Document"])
+if True: # vector search
+    nearText = {"concepts": ["what type of animal are Pythons?"]}
+    response = (
+        client.query
+        .get("Document", ["text", "url"])
+        .with_near_text(nearText)
+        #.with_limit(3)
+        .do()
+    )
+
+    result = response['data']['Get']['Document']
+    print(result)
--- server/llm.py
+++ server/llm.py
@@ -24,7 +24,11 @@ def embed_texts(texts: list[str]):
 
 def chat_completion(query:str, history: list[dict]): 
     '''
-    Given user message, message history, and vector DB, use cohere connector mode and return the api response
+    Use RAG (Cohere + Weaviate) to generate chatbot response
+
+    Given user query+message history, 
+    use query to retrieve relevant documents from Weaviate cluster,
+    feed into cohere's chat endpoint and return its response
     '''
     # TODO: fetch docs from weaviate, either by connector mode or hybrid search
     docs = []
@@ -49,55 +53,7 @@ if __name__ == "__main__":
     #soup = BeautifulSoup(response.text, 'html.parser')
     sample_document = response.text
 
-    base_prompt = """
-    You are an expert at programming in Python and thoroughly reading Python documentation that writes Python code and answers questions about Python and different libraries within.
-    The prompt you receive will always come in XML comepletely encompassed by <prompt> tags.
-    You will sometimes be provided with Python library documentation documents that you can but don't have to reference in your response, 
-    and the additional context will be provided within <context> tags with each document residing in <document> tags.
-    You should also ALWAYS respond in XML encompassed by <response> tags with any code you write residing in <code> tags and any text in <text> tags. 
-    When writing code, you should add comments that explain your thought process.
-    Here is an example of a prompt you may receive as well as a high quality response: 
-
-    <prompt>
-        How can I concatenate 2 dataframes in pandas?
-        <context>
-            <document>
-                {sample_document}
-            </document>
-        </context>
-    </prompt>
-
-    <response>
-        <text>
-            You can concatenate two Pandas DataFrames using the pd.concat() function. This function allows you to combine DataFrames either by rows (along the rows) or columns (along the columns), depending on the axis parameter.
-
-            Here's an example of concatenating two DataFrames along rows:
-        </text>
-        <code>
-            import pandas as pd
-
-            # Sample DataFrames
-            data1 = {'A': [1, 2, 3], 'B': [4, 5, 6]}
-            data2 = {'A': [7, 8, 9], 'B': [10, 11, 12]}
-
-            df1 = pd.DataFrame(data1)
-            df2 = pd.DataFrame(data2)
-
-            # Concatenate along rows
-            result = pd.concat([df1, df2], axis=0)
-
-            print(result)
-        </code>
-        <text>
-            To concatenate along columns, set the axis parameter to 1:
-        </text>
-        <code>
-            result = pd.concat([df1, df2], axis=1)
-        </code>
-    </response>
-
-    based on these guidelines, please try to answer future prompts as best as you can.
-    """
+    
 
     #print(base_prompt)
 
