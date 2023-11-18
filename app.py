9c6ba744600b8cc6ea8f2872a5cd0b5e598d169e
Marlond <marlondcriollo@gmail.com>
2023-11-18 01:34:34 -0800

    Removed Server Dependency

--- app.py
+++ app.py
@@ -2,21 +2,168 @@ import streamlit as st
 import html 
 import xml.etree.ElementTree as ET
 import requests
-import json
 import re
+import weaviate
+from dotenv import load_dotenv
+import os
+import json
+import cohere
+from cohere.responses.classify import Example
+from prompt import base_prompt
+
+
+
+load_dotenv()
+WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
+COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+
+co = cohere.Client(COHERE_API_KEY)
+
+def embed_texts(texts: list[str]): 
+    '''
+    embed texts before storing them in vector DB
+    '''
+    response = co.embed(
+        texts=texts,
+        model='embed-english-v3.0',
+        input_type='search_document'
+    )
+    return response.embeddings
+        
+def chat_completion(query:str, library:str, history:list[dict]=None): 
+    '''
+    Use RAG (Cohere + Weaviate) to generate chatbot response
+
+    Given user query+message history, 
+    use query to retrieve relevant documents from Weaviate cluster,
+    feed into cohere's chat endpoint and return its response
+    '''
+    weaviate_response = query_weaviate(query, library)
+    #print("WEAVIATE RESPONSE : " + str(weaviate_response))
+    try: 
+        docs = weaviate_response['data']['Get'][library]
+    except Exception as e: 
+        print("EXCEPTION : " + str(e))
+        docs = []
+
+    print("DOCS : " + str(docs))
+    prompt = f'{library}\n' + query # base_prompt +
+    response = co.chat(  
+        prompt,
+        model='command',   # -nightly
+        #max_tokens=200, # This parameter is optional. 
+        #return_preamble=True,
+        preamble_override=base_prompt, 
+        documents=docs,
+        chat_history=history,
+        temperature=0.5
+    )
+    response = {
+    "response_id": response.response_id,
+    "text": response.text,
+    "generation_id": response.generation_id,
+    "token_count": response.token_count,
+    "meta": response.meta,
+    "citations": response.citations,
+    "documents": response.documents,
+    "search_queries": response.search_queries
+    }
+
+    return response
+
+
+
+def get_weaviate_client(): 
+    auth_config = weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY)
+    client = weaviate.Client(
+    url="https://cohere-hack-imnt0qiv.weaviate.network",
+    auth_client_secret=auth_config, 
+    additional_headers={
+            "X-Cohere-Api-Key": COHERE_API_KEY,
+        }
+    )
+    if client.is_ready(): 
+        return client
+    return False
+
+def create_weaviate_index(index_name: str): 
+    document_schema = {
+        "class": index_name,
+        "description": f"page or chunk of software documentation for {index_name}",
+        "vectorizer": "text2vec-cohere",
+        "moduleConfig": {
+            "text2vec-cohere": {
+                "model": "embed-english-v3.0", #TODO: use embed-english-light-v3.0 if too slow
+                "truncate": "RIGHT"
+            }, 
+            "generative-cohere": {
+            "model": "command-xlarge-nightly",  # Optional - Defaults to `command-xlarge-nightly`. Can also use`command-xlarge-beta` and `command-xlarge`
+            "temperatureProperty": 0.5,  # Optional
+            #"maxTokensProperty": <maxTokens>,  # Optional
+            #"kProperty": <k>, # Optional
+            #"stopSequencesProperty": <stopSequences>, # Optional
+            #"returnLikelihoodsProperty": <returnLikelihoods>, # Optional
+            },
+        },
+        "vectorIndexConfig": {
+            "distance": "dot"
+        },
+        "properties": [
+        {
+            "name": "text",
+            "dataType": [ "text" ],
+            "description": "Doc body",
+            "moduleConfig": {
+                "text2vec-cohere": {
+                    "skip": False,
+                    "vectorizePropertyName": False
+                }
+            }
+        },
+        {
+            "name": "url",
+            "dataType": [ "text" ],
+            "moduleConfig": { "text2vec-cohere": { "skip": True } }
+        },
+        ]
+    }
+    client = get_weaviate_client()
+    client.schema.create_class(document_schema)
+
+
+def query_weaviate(query: str, index_name: str): 
+    '''
+    Vector Search weaviate database with user query
+    '''
+    # TODO: Raise certainty
+    client = get_weaviate_client()
+    nearText = {
+        "concepts": [query],
+        "certainty": 0.0
+        }
+    response = (
+        client.query
+        .get(index_name, ["text", "url"])
+        .with_near_text(nearText)
+        .with_limit(2)
+        .do()
+    )
+    return response
 
 FASTAPI_BASE_URL = "http://localhost:10000"
 
 def fetch_data_from_api(endpoint, payload=None):
-    if payload:
-        response = requests.post(f"{FASTAPI_BASE_URL}/{endpoint}", json=payload)
-    else:
-        response = requests.get(f"{FASTAPI_BASE_URL}/{endpoint}")
-    if response.status_code == 200:
-        return response.json()
-    else:
-        st.error("Failed to fetch data from the API")
-        return {}
+
+    with st.spinner('Fetching data... Please wait.'):
+        if payload:
+            response = requests.post(f"{FASTAPI_BASE_URL}/{endpoint}", json=payload)
+        else:
+            response = requests.get(f"{FASTAPI_BASE_URL}/{endpoint}")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            st.error("Failed to fetch data from the API")
+            return {}
 
 # App title and configuration
 st.set_page_config(page_title="Chatbot UI")
@@ -72,14 +219,10 @@ if "source_count" not in st.session_state:
 if prompt:
     # Construct the request payload
     sources = []
-    chat_request_payload = {
-        "message": prompt,
-        "history": st.session_state.messages,
-        "library": library
-    }
+
 
     # Send the request to the FastAPI server
-    data = fetch_data_from_api("chat/", payload=chat_request_payload)
+    data = chat_completion(prompt, library, st.session_state.messages)
     # Process the response...
     # something to not print document if they have the same URL
     json_data = data
