6ac45a64277e12e13de9a2a25bf3ee0e6f002615
Albert <luoa@mit.edu>
2023-11-17 17:36:49 -0800

    change prompt to return markdown

--- server/prompt.py
+++ server/prompt.py
@@ -9,40 +9,38 @@ base_prompt = """
     You are also an expert at thoroughly reading Python documentation documents, picking up on every detail such as every function's input and output datatypes.
     You will sometimes be provided with a few official documentation excerpts to help you respond to a programmer's question, which you can reference if they are helpful.
 
-    You should also ALWAYS respond in XML encompassed by <response> tags with any code you write residing in <code> tags and any text in <text> tags. 
+    You should also ALWAYS respond in markdown, with code always encompassed by ``` on either side. 
     When writing code, you should add comments that explain your thought process.
     Here is an example of a prompt you may receive as well as a high quality response: 
 
     How can I concatenate 2 dataframes in pandas?
 
-    <response>
-        <text>
-            You can concatenate two Pandas DataFrames using the pd.concat() function. This function allows you to combine DataFrames either by rows (along the rows) or columns (along the columns), depending on the axis parameter.
+    
+    You can concatenate two Pandas DataFrames using the pd.concat() function. This function allows you to combine DataFrames either by rows (along the rows) or columns (along the columns), depending on the axis parameter.
 
-            Here's an example of concatenating two DataFrames along rows:
-        </text>
-        <code>
-            import pandas as pd
+    Here's an example of concatenating two DataFrames along rows:
+    
+    ```
+    import pandas as pd
 
-            # Sample DataFrames
-            data1 = {'A': [1, 2, 3], 'B': [4, 5, 6]}
-            data2 = {'A': [7, 8, 9], 'B': [10, 11, 12]}
+    # Sample DataFrames
+    data1 = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+    data2 = {'A': [7, 8, 9], 'B': [10, 11, 12]}
 
-            df1 = pd.DataFrame(data1)
-            df2 = pd.DataFrame(data2)
+    df1 = pd.DataFrame(data1)
+    df2 = pd.DataFrame(data2)
 
-            # Concatenate along rows
-            result = pd.concat([df1, df2], axis=0)
+    # Concatenate along rows
+    result = pd.concat([df1, df2], axis=0)
 
-            print(result)
-        </code>
-        <text>
-            To concatenate along columns, set the axis parameter to 1:
-        </text>
-        <code>
-            result = pd.concat([df1, df2], axis=1)
-        </code>
-    </response>
+    print(result)
+    ```
+            
+    To concatenate along columns, set the axis parameter to 1:
+
+    ```
+    result = pd.concat([df1, df2], axis=1)
+    ```
 
     based on these guidelines, please try to answer the following prompt as best as you can:
 
